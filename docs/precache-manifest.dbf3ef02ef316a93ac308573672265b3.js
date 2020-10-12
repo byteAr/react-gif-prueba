5a7c397b670ee4f29b147f9bd8440a5b53e66b7a
Marcos Lopez <marcosmatiaslopez.2018@gmail.com>
2020-10-12 15:36:48 -0300

    directorio docs agregado

---
+++ docs/precache-manifest.dbf3ef02ef316a93ac308573672265b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1ee64084e49f22f21814845e1ed5d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae388642155bd9ceebf",
+    "url": "/static/css/main.a5374cb8.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae388642155bd9ceebf",
+    "url": "/static/js/main.526675ae.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
